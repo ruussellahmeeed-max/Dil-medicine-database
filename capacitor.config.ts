ca9eee4deda42941385ea20d7ac0165be128f3a8
ruussellahmeeed-max <Ruussell.ahmeeed@gmail.com>
2026-02-14 23:53:29 +0600

    fixed capacitor and added android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ruussell.dilmedicine',
+  appName: 'Dil Medicine',
+  webDir: 'dist'
+};
+
+export default config;
